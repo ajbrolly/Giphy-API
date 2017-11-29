64b7673718c4abf8fe574645698b29326e810bea
Ashlee Brolly <ashleebrolly@Ashlees-MBP.attlocal.net>
2017-11-29 13:09:31 -0500

    working js files & css files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,46 +1,110 @@
 $(document).ready(function() {
 
-var topics = ['cheeseburger', 'pasta', 'pancakes', 'cupcakes', 'coffee'];
+// List of my favoriate things
+var topics = ['Ocean', 'Spaghetti', 'Pancakes', 'Cupcakes', 'Coffee', 'Puppies', 'Yoga'];
 
-function getGiphInfo() {
-
-	var topic = $(this).attr('data-food');
-	var queryURL = 'https://www.api.giphy.com/v1/gifs/search?q=' + topic +'&api+key=puh3MW9zQIIE8RC1EiQuEBjjEg5JFVU3&limit=10';
-
-
-
-	$.ajax({
-	url: queryURL,
-	method: 'GET'
-	}).done(function(response) {
-
-		var foodDiv = $('<div class="food">');
-		console.log(response);
-	})
-
-}
-
+// Create dynamic buttons for each topic and diplay them in HTML
 function createButtons() {
-    $("#food-buttons").empty();
+	$("#fave-buttons").empty();
 
-    for (var i = 0; i < topics.length; i++) {
+	for (var j = 0; j < topics.length; j++) {
 
-      var button = $('<button>');
-      button.addClass('food');
-      button.attr('data-food', topics[i]);
-      button.text(topics[i]);
-      $("#food-buttons").append(button);
-    }
-}
+	  var button = $('<button>');
+	  button.addClass('faves');
+	  button.attr('data-fave', topics[j]);
+	  button.text(topics[j]);
+	  $("#fave-buttons").append(button);
+	}
 
-$('#add-food').click(function(event) {
-	var foodTopic = $('#food-input').val().trim();
-	topics.push(foodTopic);
-	createButtons();
-});
+	// When a button is clicked make AJAX call using search query from topics array
+	$('button').on('click', function() {
+		var currentTopic = $(this).attr('data-fave');
+		var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' +
+	        currentTopic + '&api_key=dc6zaTOxFJmzC&limit=10';
 
-$(document).click('.food', getGiphInfo);
+		$.ajax({
+		url: queryURL,
+		method: 'GET'
+		}).done(function(response) {
+
+	// Take the reponse and append to the HTML
+			console.log(response);
+			var results = response.data;
+
+			for (var i = 0; i < results.length; i++) {
+				var gifDiv = $('<div class="fave-div">');
+				gifDiv.empty();
+				var rating = results[i].rating;
+				var r = $('<div>').text('Rating: ' + rating);
+
+				var image = $('<img>');
+				image.attr('src', results[i].images.fixed_height_still.url);
+				image.attr('class', 'gif');
+				image.attr('data-state', 'still')
+				image.attr('data-still', results[i].images.fixed_height_still.url)
+				image.attr('data-animate', results[i].images.fixed_height.url)
+
+
+				gifDiv.prepend(r);
+				gifDiv.prepend(image);
+				$('#faves').prepend(gifDiv);
+				console.log(currentTopic);
+			}
+		})
+	});
+};
 
 createButtons();
 
+// Add your own Favorites to my list
+// When 'Submit' button is clicked, the user input is captured
+// and added to the topics array, a new button is created
+	$('#add-fave').on('click', function(event) {
+		event.preventDefault();
+		var newTopic = $('#fave-input').val().trim();
+		topics.push(newTopic);
+		createButtons();
+	});
+
+// Animate GIFs when they are clicked
+	// Select any element with the class name 'gif'
+	// and run function when element is clicked
+	$('.gif').on('click', function() {
+		
+		var img = $(this);
+		var state = img.attr('data-state');
+
+		// Checks to see if data-state attribute is set to 'still'
+		// If 'still' = true, then on-click the image url is changed to the URL set in data-animate
+		// The data-state attribute is changed to 'animate'
+		if (state === 'still') {
+			img.attr('src', img.attr('data-animate'));
+			img.attr('data-state', 'animate');
+		}
+
+		// If the data-state is anything other than still
+		// On-click the image url is changed to the URL save in data-still
+		// The data-state is set to still
+		else {
+			img.attr('src', img.attr('data-still'));
+			img.attr('data-state', 'still');
+		}
+
+	});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
